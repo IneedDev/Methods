b6b3cce9e3ac2ed7506819ccf24f7e63fc3e71a7
PawelRomaniuk <pawel.romaniuk@autodesk.com>
2019-01-19 09:39:14 +0100

    Methods + scanner first steps

---
+++ src/com/romaniuk/Main.java
@@ -0,0 +1,33 @@
+package com.romaniuk;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Game Over - true or false");
+        boolean gameOver = sc.nextBoolean();
+        System.out.println("Enter your score");
+        int score = sc.nextInt();
+        System.out.println("Enter completed level");
+        int levelCompleted = sc.nextInt();
+        System.out.println("Enter bonus");
+        int bonus = sc.nextInt();
+        int highScore = calculateScore(gameOver,score,levelCompleted,bonus);
+        System.out.println("Final score is "+ highScore);
+
+    }
+
+    public static int calculateScore(boolean gameOver, int score, int bonus, int levelCompleted){
+        if (gameOver){
+            int finalScore = score + (levelCompleted*bonus);
+            finalScore=finalScore+2000;
+            return finalScore;
+        }
+        return -1;
+    }
+
+}
